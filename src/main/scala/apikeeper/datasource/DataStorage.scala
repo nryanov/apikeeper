f907170f1c7c8c1d89bec2e3b95badd9877d7378
Nick <grifon52@gmail.com>
2020-02-22 20:50:39 +0300

    Update

--- src/main/scala/apikeeper/datasource/DataStorage.scala
+++ src/main/scala/apikeeper/datasource/DataStorage.scala
@@ -2,12 +2,15 @@ package apikeeper.datasource
 
 import apikeeper.Neo4jSettings
 import cats.effect.{Resource, Sync}
-import org.neo4j.driver.{AuthTokens, Driver, GraphDatabase}
+import org.neo4j.driver.{AuthToken, AuthTokens, Config, Driver, GraphDatabase, Logging}
 
 class DataStorage[F[_]](neo4jSettings: Neo4jSettings)(implicit F: Sync[F]) {
   def connect(): Resource[F, Driver] = {
-    def acquire: F[Driver] =
-      F.delay(GraphDatabase.driver(neo4jSettings.uri, AuthTokens.basic(neo4jSettings.username, neo4jSettings.password)))
+    def acquire: F[Driver] = {
+      val auth: AuthToken = AuthTokens.basic(neo4jSettings.username, neo4jSettings.password)
+      val cfg = Config.builder().withLogging(Logging.slf4j()).build()
+      F.delay(GraphDatabase.driver(neo4jSettings.uri, auth, cfg))
+    }
 
     def release(driver: Driver): F[Unit] = F.delay(driver.close())
 
--- src/main/scala/apikeeper/datasource/Migration.scala
+++ src/main/scala/apikeeper/datasource/Migration.scala
@@ -2,6 +2,7 @@ package apikeeper.datasource
 
 import apikeeper.datasource.Transactor.Tx
 import cats.data.Kleisli
+import cats.syntax.applicativeError._
 import cats.effect.Sync
 import cats.~>
 import org.neo4j.driver.Query
@@ -16,18 +17,44 @@ class Migration[F[_]](transactor: Transactor[F], queryRunner: QueryRunner[F])(im
     _ <- createRelationConstraint()
   } yield ())
 
+  def clean(): F[Unit] = transact(for {
+    _ <- dropEntityConstraint()
+    _ <- dropRelationConstraint()
+  } yield ()).handleError(_ => ()) // if constraint does not exist yet
+
+  def truncate(): F[Unit] = transact(clearDatabase())
+
+  private def clearDatabase(): Tx[F, Unit] = for {
+    _ <- Kleisli.liftF(Logger[F].info("Clear DB"))
+    _ <- queryRunner.run(new Query("match (n) detach delete n"))
+  } yield ()
+
   private def createEntityConstraint(): Tx[F, Unit] =
     for {
       _ <- Kleisli.liftF(Logger[F].info("Create unique constraint for entity"))
-      _ <- queryRunner.run(new Query("CREATE CONSTRAINT ON (e:Entity) ASSERT e.id IS UNIQUE"))
+      _ <- queryRunner.run(new Query("CREATE CONSTRAINT entity_unique_id ON (e:Entity) ASSERT e.id IS UNIQUE"))
       _ <- Kleisli.liftF(Logger[F].info("Entity constraint was created"))
     } yield ()
 
   private def createRelationConstraint(): Tx[F, Unit] =
     for {
       _ <- Kleisli.liftF(Logger[F].info("Create unique constraint for relation"))
-      _ <- queryRunner.run(new Query("CREATE CONSTRAINT ON (r:Relation) ASSERT r.id IS UNIQUE"))
+      _ <- queryRunner.run(new Query("CREATE CONSTRAINT relation_unique_id ON (r:Relation) ASSERT r.id IS UNIQUE"))
       _ <- Kleisli.liftF(Logger[F].info("Relation unique constraint was created"))
+    } yield ()
+
+  private def dropEntityConstraint(): Tx[F, Unit] =
+    for {
+      _ <- Kleisli.liftF(Logger[F].info("Drop unique constraint for entity"))
+      _ <- queryRunner.run(new Query("DROP CONSTRAINT entity_unique_id"))
+      _ <- Kleisli.liftF(Logger[F].info("Entity constraint was dropped"))
+    } yield ()
+
+  private def dropRelationConstraint(): Tx[F, Unit] =
+    for {
+      _ <- Kleisli.liftF(Logger[F].info("Drop unique constraint for relation"))
+      _ <- queryRunner.run(new Query("DROP CONSTRAINT relation_unique_id"))
+      _ <- Kleisli.liftF(Logger[F].info("Relation unique constraint was dropped"))
     } yield ()
 }
 
